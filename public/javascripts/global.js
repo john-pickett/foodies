b63a603789541f42dcaabd9b5402966f9415f386
John P <john.pickett@gmail.com>
2017-01-07 16:59:34 -0600

    change to recipes database

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -34,8 +34,9 @@ function populateTable(){
     // for each item in our JSON, add a table row and cells to the content string
     $.each(data, function(){
       tableContent += '<tr>';
-      tableContent += '<td><a href="#" class="linkshowuser" rel="' + this.username + '">' + this.username + '</a></td>';
-      tableContent += '<td>' + this.email + '</td>';
+      tableContent += '<td><a href="#" class="linkshowuser" rel="' + this.name + '">' + this.name + '</a></td>';
+      //tableContent += '<td>' + this.email + '</td>';
+      tableContent += '<td></td>';
       tableContent += '<td><a href="#" class="linkdeleteuser" rel="' + this._id + '">delete</a></td>';
       tableContent += '</tr>';
     });
