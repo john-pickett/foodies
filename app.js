adaeb5015f84b675edda61c05c408ee7555f25df
John P <john.pickett@gmail.com>
2017-01-07 12:26:04 -0600

    changes to app.js

--- app.js
+++ app.js
@@ -14,12 +14,11 @@ var users = require('./routes/users');
 
 var app = express();
 
-
+app.use(express.static(path.join(__dirname, 'public')));
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(express.static(path.join(__dirname, 'public')));
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
